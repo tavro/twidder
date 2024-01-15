55937ecd760ce89779da98af2d3fcf2f102b2ef0
Isak Horvath <isakhorvath@gmail.com>
2024-01-15 20:39:11 +0100

    LAB1: Implement validation for forms (Step 3)

--- client.js
+++ client.js
@@ -1,10 +1,40 @@
-displayView = function(){
-    // the code required to display a view
-};
+function displayView(viewId) {
+    var viewContent = document.getElementById(viewId).innerHTML;
+    document.body.innerHTML = viewContent;
+}
 
-window.onload = function(){
-    //code that is executed as the page is loaded.
-    //You shall put your own custom code here.
-    //window.alert() is not allowed to be used in your implementation.
-    window.alert("Hello TDDD97!");
-};
+document.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (e.target && e.target.id === 'login-form' || e.target && e.target.id === 'signup-form') {
+        if(validateForm(e.target.id)) {
+            console.log("working");
+        }
+    }
+});
+
+window.addEventListener('load', function () {
+    displayView('welcomeview');
+});
+
+function validateForm(formId) {
+    var form = document.forms[formId];
+    var isValid = true;
+
+    var passwordField = form.querySelector('input[type="password"]');
+    if(formId === "signup-form") {
+        var repeatPasswordField = form.querySelector('input[name="repeat_password"]');
+        if (passwordField.value !== repeatPasswordField.value) {
+            //document.getElementById("signup-error").textContent = "Passwords do not match.";
+            isValid = false;
+        }
+    }
+    
+    var minLength = 8;
+    if (passwordField.value.length < minLength) {
+        //var id = formId === "signup-form" ? "signup-error" : "login-error";
+        //document.getElementById(id).textContent = "Password must be at least " + minLength + " characters long.";
+        isValid = false;
+    }
+
+    return isValid;
+}
